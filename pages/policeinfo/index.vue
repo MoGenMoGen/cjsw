91765753fd0c4d9a6929f062109287d641c17c5d
LAPTOP-0K1DKM9E\吴林超 <794826751@qq.com>
2021-08-04 17:40:05 +0800

    8.4

--- pages/policeinfo/index.vue
+++ pages/policeinfo/index.vue
@@ -1,56 +1,136 @@
 <template>
-	<view class="table">
-		<view class="tr">
-			<text class="td">
-				1
-			</text>
-			<text class="td">
-				1
-			</text>
-			<text class="td">
-				1
-			</text>
-			<text class="td">
-				1
-			</text>
+	<view class="">
+	<swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+		<swiper-item v-for="(item,index) in swiperList" :key="index">
+			<view class="swiper-item">
+				<image :src=item mode=""></image>
+			</view>
+		</swiper-item>
+	
+	</swiper>
+	<view class="Header">
+		<view class="item">
+			时间/日期
 		</view>
-		<view class="tr">
-			<text class="td">
-				ff
-			</text>
-			<text class="td">
-				ff
-			</text>
-			<text class="td">
-				ff
-			</text>
-			<text class="td">
-				ff
-			</text>
+		<view class="item">
+			消息文本
+		</view>
+		<view class="item">
+			错误点
 		</view>
 	</view>
-</template>  
-<script></script>
-<style lang="scss">
-	.table{
-		margin:50upx auto;
-		padding:1upx;
-		.tr{
-			// padding:1upx;
-			display: flex;
-			.td{
-				display: inline-block;
-				width: 100upx;
-				height: 60upx;
-				line-height: 60upx;
-				border-left:1px solid #333 ;
-				border-top:1px solid #333 ;
-				text-align: center;
+		 
+	<view class="body" v-for="(item,index) in info" :key="index">
+		<view class="date">
+			{{item.date}} {{item.noon}}
+		</view>
+		<view class="text">
+			{{item.text}}
+		</view>
+		<view class="failPoint">
+			{{item.fail}}
+		</view>
+	</view>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				swiperList: [
+					"../../static/swiperitem.png",
+					"../../static/iconnormal.png"
+
+				],
+				info:[
+					{
+						date:"2021-07-26 03:04:27",
+						noon:"下午",
+						text:"数据超下限",
+						fail:"压差p18-p19报警"
+						
+					},
+					{
+						date:"2021-07-26 03:04:27",
+						noon:"下午",
+						text:"数据超下限",
+						fail:"压差p18-p19报警"
+						
+					},
+					
+					{
+						date:"2021-07-26 03:04:27",
+						noon:"下午",
+						text:"数据超下限",
+						fail:"p18-p19报警"
+						
+					},
+					{
+						date:"2021-07-26 03:04:27",
+						noon:"下午",
+						text:"数据超下限",
+						fail:"报警"
+						
+					},
+				]
 			}
-			.td:last-child(){
-				border:1px solid #333;
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style lang="scss">
+	.swiper {
+		width: 750upx;
+		height: 470upx;
+
+		.swiper-item {
+			width: 100%;
+			height: 470upx;
+			image {
+				width: 100%;
+				height: 100%;
 			}
 		}
 	}
-	
+	.Header{
+		background-color:#E2350A ;
+		width: 100%;
+		height: 100upx;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		.item{
+			font-size: 28upx;
+			color: #FFFFFF;
+		}
+	}
+	.body{
+		display: flex;
+		width: 100%;
+		height: 120upx;
+		background: #FFFFFF;
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+		align-items: center;
+		padding-left: 50upx
+		;
+		box-sizing: border-box;
+		color: #E2350A;
+		border-bottom:2upx solid rgba(0, 0, 0, 0.08)  ;
+		.date{
+			width: 216upx;
+			box-sizing: border-box;
+			font-size: 28upx;
+		}
+		.text{
+			margin-left: 58upx;
+			font-size: 28upx;
+		}
+		.failPoint{
+			margin-left: 54upx;
+			font-size: 28upx;
+			
+		}
+	}
 </style>
--- pages/reportform/index.vue
+++ pages/reportform/index.vue
@@ -1,6 +1,21 @@
+<!-- 报表页面 -->
 <template>
-	<view style="border: 1px solid red; position:fixed;top:88upx">
-		hhh
+	<view class="pages_reportform">
+		<view class="container">
+			<view class="hidden">
+				<view class="head_switch">
+					<text class="switch_item" v-for="(item,index) in switchList" :key="index"
+						@click="switchHead(item.id)" :class="{activeheader:item.id==currentheaderID}">
+						{{item.dictValue}}
+					</text>
+				</view>
+			</view>
+			<view class="containerBody" v-for="(item,index) in surface" :key="index">
+				<view class="containerTitle">
+					{{item.title}}
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +23,128 @@
 	export default {
 		data() {
 			return {
-				
+				switchList: [{
+						dictValue: '涂装线',
+						id: '1'
+					},
+					{
+						dictValue: '供漆',
+						id: '2'
+					}, {
+						dictValue: '机器人',
+						id: '3'
+					},
+					{
+						dictValue: '废弃处理',
+						id: '4'
+					},
+					{
+						dictValue: '冰水机房',
+						id: '5'
+					},
+					{
+						dictValue: '空压机',
+						id: '6'
+					},
+					{
+						dictValue: '安全环保',
+						id: '7'
+					}
+				],
+				currentheaderID: '1',
+				surface:[
+					{
+						title:"涂装离子风机保养记录表",
+					},
+					
+				]
 			}
 		},
 		methods: {
-			
+			switchHead(id){
+				if (this.currentheaderID != id) {
+					this.currentheaderID = id
+					uni.setStorageSync("currentheaderID", this.currentheaderID)
+					// this.getwrapperList(this.currentheaderID)
+				}
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	.pages_reportform{
+		width: 100%;
+		.container{
+			width:100%;
+		.hidden {
+			top: 0;
+			position: fixed;
+			z-index: 100;
+			background-color: #2957C4;
+			height: calc(var(--status-bar-height) +86upx);
+			// overflow: hidden;
+			width: 100%;
+		
+			.head_switch {
+				// position: fixed;
+				margin-top: calc(var(--status-bar-height) + 10upx);
+				// z-index: 100;
+				background-color: #2957C4;
+				width: 100%;
+				height: 76upx;
+				// padding-bottom: 12upx;
+				overflow-x: scroll;
+				// display: flex;
+				// align-items: center;
+				white-space: nowrap;
+		
+				.switch_item {
+					padding: 18upx 23upx;
+					font-size: 28upx;
+					font-weight: 600;
+					// color: #ecf0f1;
+					color: #fff;
+					opacity: 0.64;
+				}
+		
+				.activeheader {
+					position: relative;
+					font-size: 28upx;
+					font-weight: 600;
+					color: #fff;
+					opacity: 1;
+					transition: 0.3s;
+				}
+		
+				.activeheader::after {
+					position: absolute;
+					bottom: 8upx;
+					left: 50%;
+					transform: translateX(-50%);
+					content: "";
+					width: 50upx;
+					height: 6upx;
+					background: #fff;
+					opacity: 1;
+					border-radius: 6upx;
+				}
+			}
+		}
+		.containerBody{
+			width: 100%;
+			margin-top: 100upx;
+			padding: 62upx 0 0 0  ; 
+			box-sizing: border-box;
+			.containerTitle{
+				text-align: center;
+				font-size: 32upx;
+				color: rgba(0, 0, 0, 0.9);
+				font-weight: 600;
+				}
+		}
+		}
+		
+		
+	}
 </style>
